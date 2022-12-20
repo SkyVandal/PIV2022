e514fb668a0e491c91421c636a3a26631789380b
SkyVandal <bruno.codinha@tecnico.ulisboa.pt>
2022-12-20 17:02:20 +0000

    updates

--- pivproject2022_task1.py
+++ pivproject2022_task1.py
@@ -1,13 +1,31 @@
 import sys
-from utils.utils import *
+from utils.utils import split_video_to_frames, get_keypoints_ORB
 
+
+def project():
+    split_video_to_frames(template_dir)
+
+
+def test():
+    for i in range(1, 1000):
+        img_nr = str(i)
+        n_zeros = (4 - len(img_nr)) * '0'
+        img_path = "frames/rgb_" + n_zeros + img_nr + ".jpg"
+        k, s = get_keypoints_ORB(img_path)
+        print(img_path)
+        print(k)
+        print(s)
 
 
 
 if __name__ == "__main__":
- args = sys.argv
- template_dir = args[1]
- input_dir = args[2]
- output_dir = args[3]
+    args = sys.argv
+    template_dir = args[1]
+    input_dir = args[2]
+    output_dir = args[3]
+
+    test()
+
+
 
 
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,38 @@
 import scipy
+import numpy as np
+import cv2 as cv
+
+
+def split_video_to_frames(path):
+    vc = cv.VideoCapture(path)
+    c = 1
+
+    if vc.isOpened():
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    while rval:
+        rval, frame = vc.read()
+        img_nr = str(c)
+        n_zeros = (4 - len(img_nr)) * '0'
+        cv.imwrite('frames/rgb_' + n_zeros + img_nr + '.jpg', frame)
+        print()
+        c = c + 1
+        cv.waitKey(1)
+    vc.release()
+
+
+def get_keypoints_ORB(image_path):
+    img = cv.imread(image_path, 0)
+    # Initiate ORB detector
+    orb = cv.ORB_create()
+    # find the keypoints with ORB
+    kp = orb.detect(img, None)
+    # compute the descriptors with ORB
+    kp, des = orb.compute(img, kp)
+
+    return kp, des
 
 
 def read_keypoints_descriptors(path):
