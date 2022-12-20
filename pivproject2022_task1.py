4db24f46d30f43ff1eac97517ccb473fe67799fd
SkyVandal <bruno.codinha@tecnico.ulisboa.pt>
2022-12-20 12:05:37 +0000

    base

--- pivproject2022_task1.py
+++ pivproject2022_task1.py
@@ -1,4 +1,13 @@
-import argparse
+import sys
+from utils.utils import *
 
+
+
+
+if __name__ == "__main__":
+ args = sys.argv
+ template_dir = args[1]
+ input_dir = args[2]
+ output_dir = args[3]
 
 
